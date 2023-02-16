0877d591e7600b9522e63c8936406173ef13fd4f
MuradMagomedov <magomedoff84@gmail.com>
2023-02-16 14:33:03 +0300

    создал роут и метод для получения всех записей из таблицы продуктс

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        //
+        return response()->json([
+            '' => Product::get()
+        ]);
     }
 
     /**
