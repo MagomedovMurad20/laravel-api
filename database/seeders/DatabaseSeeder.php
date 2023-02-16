6fb70772dc24b7318576758064dccf323d052fbd
MuradMagomedov <magomedoff84@gmail.com>
2023-02-16 14:45:37 +0300

    добавил ProductSeeder и включил его в DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+
+        $this->call(
+            ProductSeeder::class,
+        );
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'title' => 'Куртка Nike',
+            'description' => 'Размер 54, материал полиэстер, сезон зимний, подкладка из шерсти',
+        ]);
+    }
+}
